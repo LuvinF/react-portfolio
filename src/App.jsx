9d996cd5efc145f627be9670aadba350d639c46b
LuvinF <luvinfernandes26@gmail.com>
2025-05-27 18:05:26 +0530

    Lazy Loading Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Feedbacks, Navbar, Works, StarsCanvas } from "./components";
+import { lazy } from "react";
+const Hero = lazy(() => import('./components/Hero'))
+const Tech = lazy(() => import('./components/Tech'))
 
 const App = () => {
   return (
